3c955194e0857165a170dd7ba04b86014ad8092d
ymmirn <134070223+ymmirn@users.noreply.github.com>
2023-05-19 22:07:32 +0900

    Update logic.py

--- template/Q1_Sort/python/logic.py
+++ template/Q1_Sort/python/logic.py
@@ -1,3 +1,11 @@
 class Logic:
     # このファイルのみ提出してください
     # この中に解答を記述してください
+    def sort(self, array):
+        n = len(array)
+        for i in range(n - 1):
+            for j in range(n - i - 1):
+                if array[j] > array[j + 1]:
+                    # 隣接する要素が逆順になっている場合、交換する
+                    array[j], array[j + 1] = array[j + 1], array[j]
+        return array
